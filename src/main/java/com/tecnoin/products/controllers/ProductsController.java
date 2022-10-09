999d4aa5980ba0cc121ead48e9109dc200a1c85b
Gerardo Flores <30551778+gerardofloresdev@users.noreply.github.com>
2022-10-08 22:21:47 -0600

    Add products controller

--- src/main/java/com/tecnoin/products/controllers/ProductsController.java
+++ src/main/java/com/tecnoin/products/controllers/ProductsController.java
@@ -1,4 +1,29 @@
 package com.tecnoin.products.controllers;
 
+import com.tecnoin.products.models.dto.ResponseDTO;
+import com.tecnoin.products.services.ProductsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import javax.annotation.security.RolesAllowed;
+
+@Controller
+@RequestMapping("/products")
 public class ProductsController {
+    @Autowired
+    ProductsService productsService;
+
+    @RolesAllowed({ "BO_ADMIN", "BO_USUARIO" })
+    @GetMapping()
+    public ResponseEntity<ResponseDTO> getProducts(
+            @RequestParam int page,
+            @RequestParam int size
+    ) {
+        ResponseEntity<ResponseDTO> response = productsService.getProducts(page, size);
+
+        return response;
+    }
 }
