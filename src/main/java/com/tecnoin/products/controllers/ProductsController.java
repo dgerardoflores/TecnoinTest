7a1b0165f932cb490df708e9f14d40bc417df82a
Gerardo Flores <30551778+gerardofloresdev@users.noreply.github.com>
2022-10-09 00:49:12 -0600

    Add endpoint to create product

--- src/main/java/com/tecnoin/products/controllers/ProductsController.java
+++ src/main/java/com/tecnoin/products/controllers/ProductsController.java
@@ -1,14 +1,12 @@
 package com.tecnoin.products.controllers;
 
+import com.tecnoin.products.models.dto.ProductDTO;
 import com.tecnoin.products.models.dto.ResponseDTO;
 import com.tecnoin.products.services.ProductsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import javax.annotation.security.RolesAllowed;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/products")
@@ -16,13 +14,21 @@ public class ProductsController {
     @Autowired
     ProductsService productsService;
 
-    @RolesAllowed({ "BO_ADMIN", "BO_USUARIO" })
     @GetMapping()
     public ResponseEntity<ResponseDTO> getProducts(
             @RequestParam int page,
             @RequestParam int size
     ) {
         ResponseEntity<ResponseDTO> response = productsService.getProducts(page, size);
+
+        return response;
+    }
+
+    @PostMapping
+    public ResponseEntity<ResponseDTO> createProduct(
+        @RequestBody ProductDTO product
+    ) {
+        ResponseEntity<ResponseDTO> response = productsService.createProduct(product);
 
         return response;
     }
