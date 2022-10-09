4991fe3ef3ec6fb7fdfd35635f2070845e6062ca
Gerardo Flores <30551778+gerardofloresdev@users.noreply.github.com>
2022-10-09 00:48:50 -0600

    Add Product DTO

---
+++ src/main/java/com/tecnoin/products/models/dto/ProductDTO.java
@@ -0,0 +1,14 @@
+package com.tecnoin.products.models.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+@Data
+public class ProductDTO {
+    @JsonInclude
+    private Long id;
+    @JsonInclude
+    private String code;
+    @JsonInclude
+    private String name;
+}
